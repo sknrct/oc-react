4de13bbaa1810c8974fe302777812b6e0103bdd7
Thibault Boucard Garda <thibault.boucardgarda@gmail.com>
2024-08-17 11:36:02 +0200

    ajout d'un about.json dans About.jsx sur conseil du mentor pour faciliter le dev si les données changent / Ajout commentaires qui ont sauté

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,34 +2,12 @@ import bannerAbout from "../assets/banner-about.png";
 import BannerTexte from "../Components/Banner/Banner";
 import Collapse from "../Components/Collapse/Collapse";
 import { useCollapse } from "../useCollapse";
+import dataAbout from "../assets/about.json";
 
 const About = () => {
   const { isActive, setIsActive } = useCollapse();
   document.title = "Kasa - A propos de nous";
-  const dataAbout = [
-    {
-      title: "Fiabilité",
-      content:
-        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
-    },
-    {
-      title: "Respect",
-      content:
-        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
-    },
-
-    {
-      title: "Service",
-      content:
-        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
-    },
-    {
-      title: "Sécurité",
-      content:
-        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
-    },
-  ];
-
+  
   return (
     <>
       <main>
@@ -37,9 +15,13 @@ const About = () => {
         <section className="section_about">
             {dataAbout.map((item, index) => (
                 <Collapse
+                // On va chercher le titre de l'item 
+                // /[^\w\s]/gi - On remplace les caractères qui ne sont pas des lettres, des chiffres ou espaces
+                // slice(0, 3) on extrait les 3 premiers caractères "nettoyé" 
+                // ca donne Fia0, Res1, Ser2, Sec3 - des clés pour chaque élément du tablo 
                 key={`${item.title.replace(/[^\w\s]/gi, "").slice(0, 3)}${index}`}
                 title={item.title}
-                content=<p>{item.content}</p>
+                content={<p>{item.content}</p>}
                 index={item.title}
                 isActive={isActive}
                 setIsActive={setIsActive}
